2ceed5459ef1ef81a2fa11c8ee07728b21787117
Daehyun Kim <kimdhyungg@gmail.com>
2024-12-14 20:03:21 +0900

    feat(nvim.lua): ignore autoformat in cubrid

---
+++ stow/cubrid/.nvim.lua
@@ -0,0 +1,23 @@
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "c", "cpp", "h", "hpp", "cmake" },
+	callback = function()
+		vim.b.autoformat = false
+	end,
+})
+
+-- Set tab settings specifically for C and C++ files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "c", "cpp", "h", "hpp", "cc", "hh" },
+	callback = function()
+		vim.bo.cindent = true
+		vim.bo.indentexpr = ""
+		vim.bo.cinoptions = "j1,f0,^-2,{2,>4,:4,n-2,(0,t0"
+		vim.bo.shiftwidth = 2
+		vim.bo.tabstop = 8
+		vim.bo.expandtab = false
+	end,
+})
+
+vim.filetype.add({
+	extension = { i = "c" },
+})

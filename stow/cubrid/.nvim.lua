88a6bcb8c79d1c30af18e4f7b9c898984c37683b
Daehyun Kim <kimdhyungg@gmail.com>
2025-01-03 18:06:30 +0900

    fix(nvim.lua): add sh

--- stow/cubrid/.nvim.lua
+++ stow/cubrid/.nvim.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "c", "cpp", "h", "hpp", "cmake" },
+	pattern = { "c", "cpp", "h", "hpp", "cmake", "sh" },
 	callback = function()
 		vim.b.autoformat = false
 	end,

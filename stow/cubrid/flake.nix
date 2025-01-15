adca52bdd8cc2cdde40ae3fe5c13dfa57a3ec62f
Daehyun Kim <kimdhyungg@gmail.com>
2025-01-15 23:29:15 +0900

    fix(flake.nix): bison 3.0.5

--- stow/cubrid/flake.nix
+++ stow/cubrid/flake.nix
@@ -1,27 +1,30 @@
 {
   description = "CUBRID development environment flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-old = {
+      url = "github:nixos/nixpkgs/0bcbb978795bab0f1a45accc211b8b0e349f1cdb";
+      flake = false; # Add this line because the old repo doesn't have flake.nix
+    };
+  };
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
-
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nixpkgs-old }:
     let
-      # Declare supported systems
       supportedSystems = [ "x86_64-linux" ];
-
-      # Helper function to generate per-system attributes
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Get pkgs for each system
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      nixpkgsOldFor =
+        forAllSystems (system: import nixpkgs-old { inherit system; });
     in {
       devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
+        let
+          pkgs = nixpkgsFor.${system};
+          pkgsOld = nixpkgsOldFor.${system};
         in {
           default = pkgs.mkShell {
-
             packages = with pkgs; [
               flex
-              bison
+              pkgsOld.bison # This will be version 3.0.5
               ncurses
               cmake
               ninja
@@ -33,11 +36,6 @@
               elfutils
               libsystemtap
             ];
-
-            shellHook = ''
-              export CC=${pkgs.gcc13}/bin/gcc
-              export CXX=${pkgs.gcc13}/bin/g++
-            '';
           };
         });
     };

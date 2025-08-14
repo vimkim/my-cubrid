10b13a5c47a32247108fafbb20180178e0f28bf1
Daehyun Kim <kimdhyungg@gmail.com>
2025-08-14 11:19:23 +0900

    fix(csql.sh): pgrep cub_server unstable

--- bin/csql.sh
+++ bin/csql.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Find the running CUBRID database name from the process list
-db=$(pgrep cub_server | xargs -I{} ps -p {} -o args= | awk '{print $2}')
+DB="${DB:-testdb}"
 
-# Connect to the database with csql
-if [ -n "$db" ]; then
-    # If a database was found, connect to it
-    echo "##### CS MODE to db: $db"
-    csql -u dba "${db}" "$@"
+echo "connecting to ... $DB"
+
+if [ "$SA" = "1" ]; then
+    echo "##### SA MODE to db: $DB"
+    csql -u dba "$DB" -S "$@"
 else
-    # If no database was found, connect to the default "testdb"
-    echo "##### SA MODE to db: testdb"
-    csql -u dba testdb -S "$@"
+    echo "##### CS MODE to db: $DB"
+    csql -u dba "$DB" "$@"
 fi

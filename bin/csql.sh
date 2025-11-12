ad564d1dadce52a1130ac2fcce15820c8c363714
Daehyun Kim <kimdhyungg@gmail.com>
2025-11-12 14:18:51 +0900

    refactor(csql.sh): prints info when running

--- bin/csql.sh
+++ bin/csql.sh
@@ -1,15 +1,20 @@
 #!/bin/env bash
 
 DB="${DB:-testdb}"
-
-echo "connecting to ... $DB"
+echo "##### csql    : $(which csql)"
+echo "##### cubrid  : $(which cubrid)"
+echo "##### DB loc  : ${CUBRID_DATABASES}"
+echo "##### DB name : $DB"
 
 # Check if DB name appears in cubrid server status
 if cubrid server status | rg -q "$DB"; then
-    echo "##### CS MODE to db: $DB"
-    exec csql -u dba "$DB" "$@"
-else
-    echo "##### SA MODE to db: $DB"
-    exec csql -u dba "$DB" -S "$@"
-fi
+  echo "##### MODE    : CS MODE"
+  echo "##### running: \$ csql -u dba $DB $*"
 
+  exec csql -u dba "$DB" "$@"
+else
+  echo "##### MODE    : SA MODE"
+  echo "##### running : \$ csql -u dba $DB -S $*"
+
+  exec csql -u dba "$DB" -S "$@"
+fi
